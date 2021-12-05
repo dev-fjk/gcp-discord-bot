d0eb7ec27e8bb7ea6599efa80324fd6e1d1597b2
dev-fjk <fjk.develop@gmail.com>
2021-12-05 15:10:20 +0900

    Discord bot の立ち上げ

--- src/main/java/com/maven/example/demo/Application.java
+++ src/main/java/com/maven/example/demo/Application.java
@@ -6,18 +6,32 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring boot application root class
+ */
 @SpringBootApplication
 @RequiredArgsConstructor
 public class Application implements CommandLineRunner {
 
     private final DiscordDao discordDao;
 
+    /**
+     * root
+     *
+     * @param args : command line program args
+     */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    /**
+     * root
+     * connect and build discord bot
+     *
+     * @param args : command line program args
+     */
     @Override
-    public void run(String... args) throws Exception {
-        discordDao.HelloWorld();
+    public void run(String... args) {
+        discordDao.connectToDiscordBot();
     }
 }
--- src/main/java/com/maven/example/demo/discord/DiscordDao.java
+++ src/main/java/com/maven/example/demo/discord/DiscordDao.java
@@ -1,9 +1,19 @@
 package com.maven.example.demo.discord;
 
+
+import com.jagrosh.jdautilities.command.CommandClient;
+import com.jagrosh.jdautilities.command.CommandClientBuilder;
+import com.maven.example.demo.discord.command.HelloCommand;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
 import org.springframework.stereotype.Controller;
 
+/**
+ * DiscordとのDAOクラス
+ */
 @Slf4j
 @Controller
 @RequiredArgsConstructor
@@ -11,9 +21,38 @@ public class DiscordDao {
 
     private final DiscordConfig discordConfig;
 
-    public void HelloWorld() {
-        System.out.println(discordConfig.getOwnerId());
-        System.out.println(discordConfig.getToken());
-        System.out.println("Hello");
+    private static final String prefix = "!";
+
+    /**
+     * DiscordBotとの接続と立ち上げを行う
+     */
+    public void connectToDiscordBot() {
+
+        log.info("Discordとの接続処理を開始します。");
+
+        try {
+            JDABuilder.createDefault(discordConfig.getToken())
+                    .addEventListeners(setUpCommand())
+                    .build();
+        } catch (final Exception e) {
+            // GCPのローカル上でしか使わないので スタックトレースは全部出す
+            e.printStackTrace();
+            throw new RuntimeException("Discordとの接続に失敗しました。");
+        }
+
+        log.info("Discord Botとの接続に成功しました。");
+    }
+
+    /**
+     * CommandClientのセットアップを行う
+     */
+    private CommandClient setUpCommand() {
+        return new CommandClientBuilder()
+                .setOwnerId(discordConfig.getOwnerId())
+                .setPrefix(prefix)
+                .setStatus(OnlineStatus.ONLINE)
+                .setActivity(Activity.listening("DiscordBot"))
+                .addCommand(new HelloCommand())
+                .build();
     }
 }
